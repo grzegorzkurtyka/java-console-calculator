8172c680d7b964d484927e78de20c3e4688549a9
Grzegorz Kurtyka <grzegorz.kurtyka@schibsted.pl>
2017-02-13 12:33:59 +0100

    Parenthesis support

---
+++ src/calc/tokenizer/token/ParenCloseToken.java
@@ -0,0 +1,11 @@
+package calc.tokenizer.token;
+
+import calc.tokenizer.token.type.ParenType;
+
+public class ParenCloseToken implements ParenType {
+
+  public String toString() {
+    return ")";
+  }
+
+}
---
+++ src/calc/tokenizer/token/ParenOpenToken.java
@@ -0,0 +1,11 @@
+package calc.tokenizer.token;
+
+import calc.tokenizer.token.type.ParenType;
+
+public class ParenOpenToken implements ParenType {
+
+  public String toString() {
+    return "(";
+  }
+
+}
